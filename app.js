a0d1c3ed11e2ddb27b5e4a121461cc0a61797bd3
weno42 <obroberts@gmail.com>
2016-01-12 17:43:53 -0500

    fixes font size issue on mobile, what a stupid feature for a mobile browser

--- app.js
+++ app.js
@@ -169,8 +169,8 @@ app.get('/search/modified', function(req, res) {
 });
 
 
-var getRandRange = function(min, max) {
-  return Math.floor(Math.random() * (max -min + 1)) + min;
+function getRandomInt(min, max) {
+  return Math.floor(Math.random()* ( max - min + 1) + min);
 }
 
 function objToString (obj) {
