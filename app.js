544ec19965f5c2d04952dedc6399daa70b8a5b05
owenroberts <obr208@nyu.edu>
2017-08-09 17:05:19 -0400

    fixes trailing spaces

--- app.js
+++ app.js
@@ -60,92 +60,89 @@ app.get('/search', function(req, res) {
 });
 
 app.get('/search/add', function(req, res) {
-    getChain(req, function(result) {
-        if (result.error)
-            res.json({
-                errormsg: "This randomly generated path was unable to be performed by the algorithm.  Please try the add path button again."
-            });
-        else res.json({ data: result });
-    });
+	getChain(req, function(result) {
+		if (result.error)
+			res.json({
+				errormsg: "This randomly generated path was unable to be performed by the algorithm.  Please try the add path button again."
+			});
+		else res.json({ data: result });
+	});
 });
 
 app.get('/search/modified', function(req, res) {
-    getChain(req, function(result) {
-        if (result.error) res.json({ errormsg: result.error });
-        else res.json({ data: result });
-    });
+	getChain(req, function(result) {
+		if (result.error) res.json({ errormsg: result.error });
+		else res.json({ data: result });
+	});
 });
 
 app.get('/def', function(req,res) {
-    def.getDef(req.query.word, function(err, result) {
-        if (err) console.log(err);
-        else res.json({ data: result });
-    })
+	def.getDef(req.query.word, function(err, result) {
+		if (err) console.log(err);
+		else res.json({ data: result });
+	})
 });
 
 function getChain(request, callback) {
-    var allsynonyms;
-    console.log(request.query.as);
-    if (request.query.as) 
-        allsynonyms = request.query.as;
-    else 
-        allsynonyms = [request.query.s];
-    var string = request.query.s + request.query.nl + request.query.e + request.query.sl;
-    string.replace(/ /g, ""); // gets rid of any spaces that might throw error, probably better way to do this
-    string = string.toLowerCase();
-    var query = {
-        queryString: string,
-        start: request.query.s,
-        end: request.query.e,
-        nodelimit: request.query.nl,
-        synonymlevel: request.query.sl,
-        searches: [{loc:"def", date: new Date()}]
-    };
-    var cacheSearch = cache.get(query.queryString);
-    if (cacheSearch == undefined) {
-        pathprovider.get(query.queryString, function(err, result) {
-            if (err) console.log(err);
-            else {
-                if (result == null) {
-                    console.log(allsynonyms);
-                    chain.makeChain(query, allsynonyms, function(err, data) {
-                        if (err) query.error = err;
-                        else query.path = data.chain;
-                        pathprovider.save(query, function(err) { console.log(err); });
-                        cache.set(query.queryString, query);
-                        callback(query);
-                    });
-                } else {
-                    pathprovider.addSearchTime(query, function(err) { console.log(err) } );
-                    cache.set(query.queryString, result);
-                    callback(result);
-                }
-            }
-        });
-    } else {
-        callback(cacheSearch);
-    }
+	var allsynonyms;
+	if (request.query.as) 
+		allsynonyms = request.query.as;
+	else 
+		allsynonyms = [request.query.s];
+	var string = request.query.s + request.query.nl + request.query.e + request.query.sl;
+	string = string.toLowerCase().replace(/ /g, "");
+	var query = {
+		queryString: string,
+		start: request.query.s.replace(/ /g, ""),
+		end: request.query.e.replace(/ /g, ""),
+		nodelimit: request.query.nl,
+		synonymlevel: request.query.sl,
+		searches: [{loc:"def", date: new Date()}]
+	};
+	var cacheSearch = cache.get(query.queryString);
+	if (cacheSearch == undefined) {
+		pathprovider.get(query.queryString, function(err, result) {
+			if (err) console.log(err);
+			else {
+				if (result == null) {
+					chain.makeChain(query, allsynonyms, function(err, data) {
+					    if (err) query.error = err;
+					    else query.path = data.chain;
+					    pathprovider.save(query, function(err) { console.log(err); });
+					    cache.set(query.queryString, query);
+					    callback(query);
+					});
+				} else {
+				 	pathprovider.addSearchTime(query, function(err) { console.log(err) } );
+				 	cache.set(query.queryString, result);
+				 	callback(result);
+				}
+			}
+		});
+	} else {
+		callback(cacheSearch);
+	}
 };
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random()* ( max - min + 1) + min);
+	return Math.floor(Math.random()* ( max - min + 1) + min);
 }
 
 function objToString (obj) {
-    var str = '';
-    for (var p in obj) {
-        if (obj.hasOwnProperty(p)) {
-            str += p + '::' + obj[p] + '\n';
-        }
-    }
-    return str;
+	var str = '';
+	for (var p in obj) {
+		if (obj.hasOwnProperty(p)) {
+			str += p + '::' + obj[p] + '\n';
+		}
+	}
+	return str;
 }
 
 
 var server = app.listen(3000, function() {
-    var host = server.address().host;
-    var port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
+	var host = server.address().host;
+	var port = server.address().port;
+	console.log('Example app listening at http://%s:%s', host, port);
 });
 var mongoUri = process.env.MONGOLAB_URI || 
   process.env.MONGOHQ_URL || 
@@ -157,23 +154,23 @@ var pathprovider = new PathProvider(mongoUri);
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 
