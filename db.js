f1e6fe0e5102400f3ef4696335120faae132279f
Owen Roberts <obroberts@gmail.com>
2022-05-08 15:15:52 -0400

    remove db logs

--- db.js
+++ db.js
@@ -56,7 +56,6 @@ ChainDb.prototype.save = function(chain, callback) {
 };
 
 ChainDb.prototype.get = function(queryString, callback) {
-	console.log('db get', queryString);
 	let collection = this.db.collection('chains');
 	collection.findOne({ queryString: queryString}, (err, result) => {
 		if (err) callback(err);
@@ -65,7 +64,6 @@ ChainDb.prototype.get = function(queryString, callback) {
 };
 
 ChainDb.prototype.addSearchTime = function(queryString, callback) {
-	console.log('db add', queryString);
 	let collection = this.db.collection('chains');
 	const search = { date: new Date()};
 	collection.updateOne(
